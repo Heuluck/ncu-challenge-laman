d066027ee1e789b9cc1cc865f50284b21d083d11
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2025-11-07 10:41:49 +0800

    feat: 初始化路由与布局

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,9 @@ export default defineConfig([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    rules: {
+      'no-unused-vars': 'warn',
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
