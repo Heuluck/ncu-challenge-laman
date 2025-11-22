c315b2e0dda9556d529c6c7b93c0d886df972db8
Heuluck <sonicth@foxmail.com>
2025-11-23 01:59:42 +0800

    feat: 更好的用户信息

--- src/layout/base.tsx
+++ src/layout/base.tsx
@@ -1,7 +1,10 @@
 import {
   AreaChartOutlined,
   DotChartOutlined,
+  GithubFilled,
   HomeFilled,
+  InfoCircleFilled,
+  LogoutOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 import {
@@ -12,9 +15,9 @@ import {
   SettingDrawer,
   type ProSettings,
 } from "@ant-design/pro-components";
-import { ConfigProvider } from "antd";
+import { Avatar, ConfigProvider, Tooltip } from "antd";
 import { useState } from "react";
-import { Link, Outlet, useLocation } from "react-router";
+import { Link, Outlet, useLocation, useNavigate } from "react-router";
 import useUserStore from "../store/user";
 
 const routeList = {
@@ -50,11 +53,15 @@ const routeList = {
 function BaseLayout() {
   const [settings, setSetting] = useState<Partial<ProSettings> | undefined>();
   const location = useLocation();
+  const navigate = useNavigate();
   const userStore = useUserStore();
   return (
     <div id="pro-layout-container">
       <ProConfigProvider hashed={false}>
         <ConfigProvider
+          tooltip={{
+            unique: true,
+          }}
           getTargetContainer={() => {
             return (
               document.getElementById("pro-layout-container") || document.body
@@ -72,11 +79,70 @@ function BaseLayout() {
               item.path ? <Link to={item.path}>{dom}</Link> : dom
             }
             route={routeList}
-            menuFooterRender={() => (
-              <div className="text-center">
-                {userStore.userData.username ? userStore.userData.username : <Link to="/login">登录</Link>}
-              </div>
-            )}
+            menuFooterRender={(props) => {
+              if (props?.isMobile) return [];
+              if (props?.collapsed) {
+                return <></>;
+              }
+              return [
+                <div key={1} className="text-center">
+                  {userStore.userData.username ? (
+                    <div className="flex items-center justify-between px-2">
+                      <div className="flex items-center gap-2">
+                        <Avatar
+                          src="https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg"
+                          size="small"
+                        />
+                        <div
+                          style={{
+                            fontSize: "14px",
+                            marginInlineEnd: "32px",
+                          }}
+                        >
+                          {userStore.userData.username}
+                        </div>
+                      </div>
+                      <div className="flex space-x-0.5">
+                        <Tooltip
+                          title={`${userStore.userData.department} - ${userStore.userData.userPermission}`}
+                        >
+                          <button className="cursor-pointer rounded-sm px-1 py-0.5 transition-all duration-300 hover:bg-gray-300">
+                            <InfoCircleFilled key="InfoCircleFilled" />
+                          </button>
+                        </Tooltip>
+                        <Tooltip title="项目仓库">
+                          <button className="cursor-pointer rounded-sm px-1 py-0.5 transition-all duration-300 hover:bg-gray-300">
+                            <a
+                              style={{ color: "#868686" }}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              href="https://github.com/heuluck/ncu-challenge-laman-DB"
+                            >
+                              <GithubFilled key="GithubFilled" />
+                            </a>
+                          </button>
+                        </Tooltip>
+                        <Tooltip title="退出登录">
+                          <button
+                            className="cursor-pointer rounded-sm px-1 py-0.5 transition-all duration-300 hover:bg-gray-300"
+                            onClick={() => {
+                              localStorage.removeItem("authToken");
+                              localStorage.removeItem("userData");
+                              userStore.removeAllData();
+                              navigate("/login");
+                            }}
+                          >
+                            <LogoutOutlined key="QuestionCircleFilled" />
+                          </button>
+                        </Tooltip>
+                      </div>
+                    </div>
+                  ) : (
+                    <Link to="/login">登录</Link>
+                  )}
+                </div>,
+              ];
+            }}
           >
             <PageContainer>
               <ProCard>
