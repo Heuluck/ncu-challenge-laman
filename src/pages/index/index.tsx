4676bcfd063faf8d0218291f83608994f9708393
Heuluck <sonicth@foxmail.com>
2025-11-23 23:10:39 +0800

    feat: 编辑病人

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,8 @@
-import type { ActionType, ProColumns } from "@ant-design/pro-components";
+import type {
+  ActionType,
+  ProColumns,
+  ProFormInstance,
+} from "@ant-design/pro-components";
 import { PlusOutlined } from "@ant-design/icons";
 import {
   ProTable,
@@ -12,168 +16,67 @@ import {
   ProFormDigit,
   ProFormDateRangePicker,
   ProFormSlider,
+  ProFormTextArea,
 } from "@ant-design/pro-components";
 import { Button, Form, message } from "antd";
 import {
   CreatePatient,
   GetPatientGroups,
   GetPatientList,
+  UpdatePatient,
 } from "../../api/patient/patient";
 import type { PatientListItem } from "../../api/patient/patient-res";
 import dayjs from "dayjs";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const requestGroups =async () => {
-      try {
-        const data = await GetPatientGroups();
-        const groups = data.data?.groups || [];
-        return groups.map((group) => ({
-          label: group,
-          value: group,
-        }));
-      } catch (e) {
-        console.error("获取组别列表失败:", e);
-      }
-      return [];
-    }
-
-const columns: ProColumns<PatientListItem>[] = [
-  {
-    title: "编号",
-    dataIndex: "id",
-    key: "id",
-    fixed: "left",
-    width: 60,
-    sorter: (a, b) => (a?.id ?? 0) - (b?.id ?? 0),
-    hideInSearch: true,
-  },
-  { title: "姓名", dataIndex: "name", key: "name", fixed: "left", width: 70 },
-  { title: "缩写", dataIndex: "abbr", key: "abbr" },
-  { title: "性别", dataIndex: "gender", key: "gender" },
-  {
-    title: "年龄",
-    dataIndex: "age",
-    key: "age",
-    renderFormItem: () => (
-      <ProFormSlider
-        min={0}
-        max={150}
-        range
-        fieldProps={{ defaultValue: [0, 150] }}
-      />
-    ),
-    search: {
-      transform: (value) => ({
-        ageMin: value[0],
-        ageMax: value[1],
-      }),
-    },
-  },
-  {
-    title: "住院/门诊",
-    dataIndex: "inpatientOutpatient",
-    key: "inpatientOutpatient",
-    width: 80,
-    valueType: "select",
-    valueEnum: {
-      住院: { text: "住院" },
-      门诊: { text: "门诊" },
-    },
-  },
-  {
-    title: "组别",
-    dataIndex: "group",
-    key: "group",
-    valueType: "select",
-    width: 100,
-    renderFormItem(_, config, form) {
-      return <ProFormSelect showSearch {...config} {...form} />;
-    },
-    request: requestGroups,
-  },
-  {
-    title: "时间",
-    dataIndex: "time",
-    key: "time",
-    valueType: "dateTime",
-    renderFormItem: () => (
-      <ProFormDateRangePicker fieldProps={{ allowEmpty: true }} />
-    ),
-    search: {
-      transform: (value) => ({
-        timeStart: Math.floor(dayjs(value[0]).valueOf() / 1000),
-        timeEnd: Math.floor(dayjs(value[1]).valueOf() / 1000),
-      }),
-    },
-    width: 120,
-    render: (_, record) =>
-      record.time
-        ? dayjs(Number(record.time) * 1000)
-            .locale("zh-cn")
-            .format("YYYY-MM-DD")
-        : undefined,
-  },
-  { title: "流水号", dataIndex: "serialNo", key: "serialNo" },
-  { title: "病案号", dataIndex: "caseNo", key: "caseNo" },
-  { title: "诊断", dataIndex: "diagnosis", key: "diagnosis", width: 150 },
-  {
-    title: "是否已检",
-    dataIndex: "isTested",
-    key: "isTested",
-    valueType: "select",
-    valueEnum: {
-      true: { text: "是" },
-      false: { text: "否" },
-    },
-  },
-  {
-    title: "TNM 分期",
-    key: "tnmStage",
-    children: [
-      { title: "T", dataIndex: "tStage", key: "tStage" },
-      { title: "N", dataIndex: "nStage", key: "nStage" },
-      { title: "M", dataIndex: "mStage", key: "mStage" },
-    ],
-    hideInSearch: true,
-  },
-  {
-    title: "分期组合",
-    key: "stageCombination",
-    children: [{ title: "分期", dataIndex: "stage", key: "stage" }],
-    hideInSearch: true,
-  },
-  {
-    title: "采样前是否接受治疗",
-    dataIndex: "preTreatment",
-    key: "preTreatment",
-    valueType: "select",
-    valueEnum: {
-      true: { text: "是" },
-      false: { text: "否" },
-    },
-  },
-  { title: "接受何种治疗", dataIndex: "treatmentType", key: "treatmentType" },
-  {
-    title: "备注",
-    dataIndex: "memo",
-    key: "memo",
-    width: 120,
-    ellipsis: true,
-  },
-];
+const requestGroups = async () => {
+  try {
+    const data = await GetPatientGroups();
+    const groups = data.data?.groups || [];
+    return groups.map((group) => ({
+      label: group,
+      value: group,
+    }));
+  } catch (e) {
+    console.error("获取组别列表失败:", e);
+  }
+  return [];
+};
 
 function IndexPage() {
   const actionRef = useRef<ActionType>(null);
+  const formRef = useRef<ProFormInstance>(null);
+  const [formOpen, setFormOpen] = useState(false);
+  const [editData, setEditData] = useState({
+    id: -1,
+    isEdit: false,
+  });
+
+  useEffect(() => {
+    formRef.current?.resetFields();
+  }, [editData]);
 
   const renderCreateAction = () => {
     const [form] = Form.useForm<{ name: string; company: string }>();
 
     return (
       <DrawerForm<Omit<PatientListItem, "id">>
-        title="添加病人数据"
+        open={formOpen}
+        onOpenChange={setFormOpen}
+        title={editData.isEdit ? "编辑病人数据" : "添加病人数据"}
+        formRef={formRef}
         form={form}
         trigger={
-          <Button type="primary">
+          <Button
+            type="primary"
+            onClick={() => {
+              if (editData.isEdit)
+                setEditData({
+                  id: -1,
+                  isEdit: false,
+                });
+            }}
+          >
             <PlusOutlined />
             添加数据
           </Button>
@@ -184,11 +87,24 @@ function IndexPage() {
         }}
         submitTimeout={2000}
         onFinish={async (values) => {
+          if (editData.isEdit)
+            try {
+              values.time = dayjs(values.time).valueOf() / 1000;
+              await UpdatePatient({ id: editData.id, ...values });
+              actionRef.current?.reload();
+              message.success("修改成功");
+              formRef.current?.resetFields();
+              return true;
+            } catch (e) {
+              message.error("修改失败，请重试");
+              return false;
+            }
           try {
             values.time = dayjs(values.time).valueOf() / 1000;
             await CreatePatient(values);
             actionRef.current?.reload();
             message.success("提交成功");
+            formRef.current?.resetFields();
             return true;
           } catch (e) {
             message.error("提交失败，请重试");
@@ -320,14 +236,54 @@ function IndexPage() {
             if (isTested) {
               return (
                 <>
-                  <ProFormSelect
-                    name="group"
-                    width="md"
-                    label="组别"
-                    placeholder="请输入组别"
-                    showSearch
-                    request={requestGroups}
+                  <ProFormRadio.Group
+                    name="isNewGroup"
+                    radioType="button"
+                    label="是否为新组别"
+                    options={[
+                      { label: "新建组别", value: true },
+                      { label: "已有组别", value: false },
+                    ]}
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
                   />
+                  <ProFormDependency name={["isNewGroup"]}>
+                    {({ isNewGroup }) => {
+                      if (isNewGroup) {
+                        return (
+                          <ProFormText
+                            name="group"
+                            width="md"
+                            label="组别"
+                            placeholder="请输入组别"
+                            rules={[
+                              {
+                                required: true,
+                              },
+                            ]}
+                          />
+                        );
+                      }
+                      return (
+                        <ProFormSelect
+                          name="group"
+                          width="md"
+                          label="组别"
+                          placeholder="请输入组别"
+                          showSearch
+                          request={requestGroups}
+                          rules={[
+                            {
+                              required: true,
+                            },
+                          ]}
+                        />
+                      );
+                    }}
+                  </ProFormDependency>
                   <ProForm.Group>
                     <ProFormText
                       name="tStage"
@@ -375,51 +331,204 @@ function IndexPage() {
             return null;
           }}
         </ProFormDependency>
-        <ProForm.Group>
-          <ProFormRadio.Group
-            name="preTreatment"
-            radioType="button"
-            label="采样前是否接受治疗"
-            options={[
-              { label: "是", value: true },
-              { label: "否", value: false },
-            ]}
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          />
-          <ProFormDependency name={["preTreatment"]}>
-            {({ preTreatment }) => {
-              if (preTreatment) {
-                return (
-                  <ProFormText
-                    name="treatmentType"
-                    width="md"
-                    label="接受何种治疗"
-                    placeholder="请输入接受的治疗类型"
-                    rules={[
-                      {
-                        required: true,
-                      },
-                    ]}
-                  />
-                );
-              }
-              return null;
-            }}
-          </ProFormDependency>
-        </ProForm.Group>
-        <ProFormText
+        <ProFormRadio.Group
+          name="preTreatment"
+          radioType="button"
+          label="采样前是否接受治疗"
+          options={[
+            { label: "是", value: true },
+            { label: "否", value: false },
+          ]}
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        />
+        <ProFormDependency name={["preTreatment"]}>
+          {({ preTreatment }) => {
+            if (preTreatment) {
+              return (
+                <ProFormText
+                  name="treatmentType"
+                  width="md"
+                  label="接受何种治疗"
+                  placeholder="请输入接受的治疗类型"
+                  rules={[
+                    {
+                      required: true,
+                    },
+                  ]}
+                />
+              );
+            }
+            return null;
+          }}
+        </ProFormDependency>
+        <ProFormTextArea
           name="memo"
-          width="lg"
           label="备注"
           placeholder="请输入备注信息"
         />
       </DrawerForm>
     );
   };
+
+  const columns: ProColumns<PatientListItem>[] = [
+    {
+      title: "编号",
+      dataIndex: "id",
+      key: "id",
+      fixed: "left",
+      width: 45,
+      hideInSearch: true,
+    },
+    { title: "姓名", dataIndex: "name", key: "name", fixed: "left", width: 70 },
+    { title: "缩写", dataIndex: "abbr", key: "abbr" },
+    { title: "性别", dataIndex: "gender", key: "gender" },
+    {
+      title: "年龄",
+      dataIndex: "age",
+      key: "age",
+      renderFormItem: () => (
+        <ProFormSlider
+          min={0}
+          max={150}
+          range
+          fieldProps={{ defaultValue: [0, 150] }}
+        />
+      ),
+      search: {
+        transform: (value) => ({
+          ageMin: value[0],
+          ageMax: value[1],
+        }),
+      },
+    },
+    {
+      title: "住院/门诊",
+      dataIndex: "inpatientOutpatient",
+      key: "inpatientOutpatient",
+      width: 80,
+      valueType: "select",
+      valueEnum: {
+        住院: { text: "住院" },
+        门诊: { text: "门诊" },
+      },
+    },
+    {
+      title: "组别",
+      dataIndex: "group",
+      key: "group",
+      valueType: "select",
+      width: 100,
+      renderFormItem(_, config, form) {
+        return <ProFormSelect showSearch {...config} {...form} />;
+      },
+      request: requestGroups,
+    },
+    {
+      title: "时间",
+      dataIndex: "time",
+      key: "time",
+      valueType: "dateTime",
+      renderFormItem: () => (
+        <ProFormDateRangePicker fieldProps={{ allowEmpty: true }} />
+      ),
+      search: {
+        transform: (value) => ({
+          timeStart: Math.floor(dayjs(value[0]).valueOf() / 1000),
+          timeEnd: Math.floor(dayjs(value[1]).valueOf() / 1000),
+        }),
+      },
+      width: 120,
+      render: (_, record) =>
+        record.time
+          ? dayjs(Number(record.time) * 1000)
+              .locale("zh-cn")
+              .format("YYYY-MM-DD")
+          : undefined,
+    },
+    { title: "流水号", dataIndex: "serialNo", key: "serialNo" },
+    { title: "病案号", dataIndex: "caseNo", key: "caseNo" },
+    { title: "诊断", dataIndex: "diagnosis", key: "diagnosis", width: 150 },
+    {
+      title: "是否已检",
+      dataIndex: "isTested",
+      key: "isTested",
+      valueType: "select",
+      valueEnum: {
+        true: { text: "是" },
+        false: { text: "否" },
+      },
+    },
+    {
+      title: "TNM 分期",
+      key: "tnmStage",
+      children: [
+        { title: "T", dataIndex: "tStage", key: "tStage" },
+        { title: "N", dataIndex: "nStage", key: "nStage" },
+        { title: "M", dataIndex: "mStage", key: "mStage" },
+      ],
+      hideInSearch: true,
+    },
+    {
+      title: "分期组合",
+      key: "stageCombination",
+      children: [{ title: "分期", dataIndex: "stage", key: "stage" }],
+      hideInSearch: true,
+    },
+    {
+      title: "采样前是否接受治疗",
+      dataIndex: "preTreatment",
+      key: "preTreatment",
+      valueType: "select",
+      valueEnum: {
+        true: { text: "是" },
+        false: { text: "否" },
+      },
+    },
+    { title: "接受何种治疗", dataIndex: "treatmentType", key: "treatmentType" },
+    {
+      title: "备注",
+      dataIndex: "memo",
+      key: "memo",
+      width: 120,
+      ellipsis: true,
+    },
+    {
+      title: "操作",
+      valueType: "option",
+      key: "option",
+      width: 120,
+      render: (_text, record, _, _action) => [
+        <Button
+          key="edit"
+          size="small"
+          onClick={() => {
+            setEditData({
+              id: record.id,
+              isEdit: true,
+            });
+            setFormOpen(true);
+            setTimeout(() => {
+              console.log(typeof record.isTested);
+              formRef.current?.setFieldsValue({
+                ...record,
+                isNewGroup: record.group ? false : true,
+                time: record.time ? dayjs(Number(record.time) * 1000) : null,
+              });
+            }, 0);
+          }}
+        >
+          编辑
+        </Button>,
+        <Button key="delete" size="small" danger>
+          删除
+        </Button>,
+      ],
+    },
+  ];
 
   return (
     <ProTable<PatientListItem>
@@ -442,7 +551,7 @@ function IndexPage() {
       search={{
         labelWidth: "auto",
       }}
-      scroll={{ x: 1600 }}
+      scroll={{ x: 1650 }}
       rowKey="key"
       pagination={{
         showQuickJumper: true,
